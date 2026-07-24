5c6ed0e7bb4db9af27d81437cdd080abe6bf05ea
Beenish Latif <beenishlatif1026@gmail.com>
2026-07-24 17:35:23 +0500

    add

--- controllers/stats.controller.js
+++ controllers/stats.controller.js
@@ -19,7 +19,7 @@
  * ------------------------------------------------------------------
  */
 
-import StatsSnapshot from "../models/StatsSnapshot.js";
+import StatsSnapshot from "../models/Statssnapshot.js";
 import { getInjMarketData, getMarketChart } from "../services/coingecko.service.js";
 import {
   getStakingInfo,
